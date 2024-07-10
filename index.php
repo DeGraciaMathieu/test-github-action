af2205065697bde991aca990c96b052b28be2ba7
DeGraciaMathieu <degracia.mathieu@gmail.com>
2024-07-10 18:30:32 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,18 @@
 <?php
 
-$input = $argv[1];
+$phpmetricsUrl = 'https://github.com/phpmetrics/PhpMetrics/releases/download/v2.7.3/phpmetrics.phar';
 
-echo $input;
+$phpmetricsPath = '/tmp/phpmetrics.phar';
+
+file_put_contents($phpmetricsPath, file_get_contents($phpmetricsUrl));
+
+chmod($phpmetricsPath, 0755);
+
+//$directory = $argv[1] ?? '.';
+
+$command = "php $phpmetricsPath .";
+
+$output = shell_exec($command);
+
+echo $output;
+
